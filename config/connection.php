f9762dabd69a5b330f4a6d14204be3f30f5307bb
kimgabor <alu.2511253@itesrenedescartes.edu.mx>
2026-07-17 10:12:15 -0600

    Se elimino el gitignore

---
+++ config/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$host     = 'localhost';
+$dbname   = 'reclutamiento_vacantes';
+$user     = 'root';
+$password = '';
+
+
+$conn = new mysqli($host, $user, $password, $dbname, 3307);
+
+$conn->set_charset('utf8mb4');
+
+if ($conn->connect_error) {
+    die(json_encode(['success' => false, 'message' => 'Error de conexión: ' . $conn->connect_error]));
+}
